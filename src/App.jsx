643605e3a87b43537f91568e6a4cf0d077c88e04
anhdung136 <nad1362003@gmail.com>
2024-06-20 21:04:39 +0700

    feat: create member ui

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
