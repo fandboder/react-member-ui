f5f281f813a2ee374e70d9aa819ffd98aef555c1
Minh <elevenine00@gmail.com>
2024-07-04 16:07:58 +0700

    fix: config vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-//import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import Checkout from "./pages/Checkout/Checkout";
 import Welcome from "./pages/Welcome/Welcome";
 import BaseLayout from "./layout/BaseLayout";
-//import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  //const location = useLocation();  đưa import useLocation  vào chung với route và routes
 
   return (
     <>
-      {/*location.pathname !== "/" && <NavBar />*/}
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route element={<BaseLayout />}>
@@ -21,7 +17,6 @@ const App = () => {
         </Route>
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
-      {/*{location.pathname !== "/" && <Footer />}*/}
     </>
   );
 };
