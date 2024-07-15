13666553a607d6cbc0c95c65c4cf71c25d994db2
anhdung136 <nad1362003@gmail.com>
2024-07-15 10:52:19 +0700

    can choose toppings when adding products to the cart, distinguish the same product but order 2 times with 2 different types of toppings

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,24 +1,39 @@
 import "./Welcome.css";
 import NavBar from "../../components/ui/navbar";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react"; // Import useState and useEffect
 
 const Welcome = () => {
+  const [currentDate, setCurrentDate] = useState(new Date()); // State for current date/time
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const ictOffset = 7 * 60; // ICT timezone offset in minutes
+      const ictTime = new Date(now.getTime() + ictOffset * 60 * 1000);
+      setCurrentDate(ictTime);
+    }, 1000); // Update every second
+
+    return () => clearInterval(interval); // Clean up interval on unmount
+  }, []);
+
+  const formattedDate = currentDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
   return (
     <div className="creator">
       <div className="c-heading">
         <div className="c-heading__top">
-          <h2 className="text-default">Site of the Day - Jun 17, 2024</h2>
+          <h2 className="text-default">Site of the Day - {formattedDate}</h2>
         </div>
         <div className="c-heading__middle">
           <h1 className="heading-1">
-            <a
-              href="/home"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redis{" "}
+            <a href="/home" target="_blank" rel="noopener noreferrer">
+              Fast{" "}
               <mark className="rounded-md bg-white-100 text-orange-500 hover:bg-orange-500 hover:text-yellow-50">
-                Agency
+                Food
               </mark>
             </a>
           </h1>
@@ -29,7 +44,8 @@ const Welcome = () => {
         </div>
       </div>
       <div className="center-button">
-        <Link to="home"
+        <Link
+          to="home"
           className="button-container rounded-full border-2 border-orange-500 text-orange-500 hover:bg-orange-500 hover:text-white-100"
         >
           ORDER NOW!
